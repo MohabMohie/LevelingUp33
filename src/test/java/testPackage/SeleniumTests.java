a3a3e3ff721075fc19f8bfad4984eccc6cb73c78
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2025-11-28 14:32:09 +0200

    adding sample test case

---
+++ src/test/java/testPackage/SeleniumTests.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTests {
+    @Test
+    public void sampleSeleniumTest() {
+        System.out.println("This is a sample Selenium test.");
+    }
+}
